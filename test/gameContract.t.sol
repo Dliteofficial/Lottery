da698dcfb98d4a8acf0debe316a5a8da3d208339
Dliteofficial <opeyemi4adewole@gmail.com>
2022-11-17 01:20:22 +0100

    Begin Testing...

--- test/gameContract.t.sol
+++ test/gameContract.t.sol
@@ -6,5 +6,19 @@ import "src/gameContract.sol";
 
 contract gameContractTest is Test {
 
-    
+    gameContract game;
+    address attacker;
+    address[] users;
+
+    function setUp() public {
+        game = new gameContract();
+        vm.label (address(game), "Game Contract!!!");
+
+        attacker = address(10);
+        vm.label(attacker, "Attacker");
+
+        for(uint i = 1; i < 9; i++){
+            users[i] = address(i);
+        }
+    }
 }
